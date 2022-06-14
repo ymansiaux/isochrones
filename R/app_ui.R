c46ad695336453b5997f4b038c0ff7aa0b281821
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2022-06-14 13:50:32 +0200

    Alignement des éléments dans la grid OK

--- R/app_ui.R
+++ R/app_ui.R
@@ -43,95 +43,111 @@ app_ui <- function(request) {
         "grid basique",
         
         div(class="parent",
-            div(class="div1", style = "background : blue;", "div1"),
-            div(class="div2", style = "background : red;", "div2"),
-            div(class="div3", style = "background : pink;", "div3"),
-            div(class="div4", style = "background : green;", 
+            div(class="div1 border", "Décrire l'objectif"),
+            div(class="div2 border",  
+                
+                div(class = "container",
+                    style = "display:flex;
+                             flex-direction : column;
+                             min-height:100%;
+                             ",
+                  
+                  div(
+                    h4("Préparation du géocodage"),
+                    tags$br(),
+                    textInput(inputId = "adress",
+                              label = "Adresse à géocoder",
+                              value = "27 rue Jean Fleuret, 33000 Bordeaux",
+                              placeholder = "Ex : 35 rue Neuve, 33000 Bordeaux"),
+                    
+                    actionButton(inputId = "run_geocoding",
+                                 label = "Lancer le géocodage"
+                    )
+                  ),
+                  
+                  div(style = "margin-bottom: auto;
+                               margin-top: auto;",
+                    h4("Résultat géocodage"),
+                    tags$br(),
+                    DTOutput("geocoding_table"),
+                    tags$br(),
+                    actionButton(inputId = "run_geocoding",
+                                 label = "Valider la sélection et passer à l'onglet suivant"
+                    )
+                  )
+                )
+            ),
+    
+    
+            div(class="div3 border", style = "min-height:100%;", 
+                leafletOutput("map_geocoding", width="100%", height="100%")
+                ),
+    
+    
+            div(class="div4 border", 
                 includeHTML(app_sys("app/www/footer.html"))
-
+                
             )
         )
+      ),
+
+      tabPanel(
+        "calculer un isochrone",
+        div(class = "content",
+            
+            
+            sliderInput(inputId = "isochrone_size", 
+                        label = "Taille de l'isochone (minutes)",
+                        min = 1,
+                        max = 60,
+                        step = 5,
+                        value = 15),
+            
+            selectInput(inputId = "osrm.profile",
+                        label = "mode de déplacement",
+                        choices = c("piéton" = "foot",
+                                    "vélo" = "bike",
+                                    "auto" = "car")
+                        
+            ),
+            
+            actionButton(inputId = "isochrone_computing", label = "Lancer calcul de l'isochrone")
+        )
+      ),
+      
+      tabPanel(
+        "équipements",
+        div(class = "content",
+            
+            h2("Afficher les équipements présents dans l'isochrone"),
+            h3("Patienter jusqu'à l'affichage de l'isochrone sur la carte pour lancer la suite"),
+            
+            selectizeInput(inputId = "equipement_theme",
+                           label = "Catégories d'équipements",
+                           choices = c("A : Enseignement divers et formation" = "A",
+                                       "B : Santé et action sociale" = "B",
+                                       "C : Sport - Loisir - Socio-éducatif" = "C",
+                                       "D: Culture - Patrimoine" = "D",
+                                       "E : Administration" = "E",
+                                       "F : Services" = "F",
+                                       "G : Sécurité" = "G",
+                                       "H : Espace vert ou espace urbain public" = "H",
+                                       "J : Déplacements" = "J",
+                                       "K : Production et transformation d'énergie - assainissement - environnement" = "K",
+                                       "L : Cultuel" = "L",
+                                       "M : Métropole" = "M",
+                                       "N : Sénior" = "N",
+                                       "O : Commune" = "O",
+                                       "P : Petite enfance" = "P"),
+                           multiple = TRUE
+            ),
+            
+            actionButton(inputId = "equipements_computing", label = "Calculer les équipements présents dans la zone")
+        ),
+        column(width = 6, h5("titi")
+
+        ) 
       )
-,
-
-# tabPanel(
-#   "Accueil",
-#   div(class = "content",
-#       
-#       fluidRow(
-#         
-#         column(width = 6,
-#                textInput(inputId = "adress",
-#                          label = "Adresse à géocoder",
-#                          value = "27 rue Jean Fleuret, 33000 Bordeaux",
-#                          placeholder = "Ex : 35 rue Neuve, 33000 Bordeaux"),
-#                
-#                actionButton(inputId = "run_geocoding",
-#                             label = "Lancer le géocodage"
-#                )
-#         )
-#       )
-#   )
-# ),
-
-tabPanel(
-  "calculer un isochrone",
-  div(class = "content",
-      
-      
-      sliderInput(inputId = "isochrone_size", 
-                  label = "Taille de l'isochone (minutes)",
-                  min = 1,
-                  max = 60,
-                  step = 5,
-                  value = 15),
-      
-      selectInput(inputId = "osrm.profile",
-                  label = "mode de déplacement",
-                  choices = c("piéton" = "foot",
-                              "vélo" = "bike",
-                              "auto" = "car")
-                  
-      ),
-      
-      actionButton(inputId = "isochrone_computing", label = "Lancer calcul de l'isochrone")
-  )
-),
-
-tabPanel(
-  "équipements",
-  div(class = "content",
-      
-      h2("Afficher les équipements présents dans l'isochrone"),
-      h3("Patienter jusqu'à l'affichage de l'isochrone sur la carte pour lancer la suite"),
-      
-      selectizeInput(inputId = "equipement_theme",
-                     label = "Catégories d'équipements",
-                     choices = c("A : Enseignement divers et formation" = "A",
-                                 "B : Santé et action sociale" = "B",
-                                 "C : Sport - Loisir - Socio-éducatif" = "C",
-                                 "D: Culture - Patrimoine" = "D",
-                                 "E : Administration" = "E",
-                                 "F : Services" = "F",
-                                 "G : Sécurité" = "G",
-                                 "H : Espace vert ou espace urbain public" = "H",
-                                 "J : Déplacements" = "J",
-                                 "K : Production et transformation d'énergie - assainissement - environnement" = "K",
-                                 "L : Cultuel" = "L",
-                                 "M : Métropole" = "M",
-                                 "N : Sénior" = "N",
-                                 "O : Commune" = "O",
-                                 "P : Petite enfance" = "P"),
-                     multiple = TRUE
-      ),
-      
-      actionButton(inputId = "equipements_computing", label = "Calculer les équipements présents dans la zone")
-  ),
-  column(width = 6, h5("titi")
-         # leafletOutput("map_geocoding")
-         
-  ) 
-)
     )
   )
 }
@@ -163,16 +179,17 @@ golem_add_external_resources <- function(){
               .div1 { grid-area: 1 / 1 / 2 / 5; }
 .div2 { grid-area: 2 / 1 / 6 / 3; }
 .div3 { grid-area: 2 / 3 / 6 / 5; }
-.div4 { grid-area: 6 / 1 / 7 / 5; }"
+.div4 { grid-area: 6 / 1 / 7 / 5; }
+.border {border: 2px solid white;}"
     ),
-    # Note the wrapping of the string in HTML()
-    favicon(),
-    bundle_resources(
-      path = app_sys('app/www'),
-      app_title = 'isochrones'
-    )
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+# Note the wrapping of the string in HTML()
+favicon(),
+bundle_resources(
+  path = app_sys('app/www'),
+  app_title = 'isochrones'
+)
+# Add here other external resources
+# for example, you can add shinyalert::useShinyalert() 
   )
 }
 
